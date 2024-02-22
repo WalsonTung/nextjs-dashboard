2bf8a08c90a21020c78568647a4f2213ad1bd821
donghx <20022889@163.com>
2024-02-22 18:11:40 +0800

    添加数据获取、展示和编辑功能。

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+// const nextConfig = {
+//   async rewrites() {
+//     return {
+//       fallback: [
+//         {
+//           source: '/:path*',
+//           destination: `http://10.74.46.23:8080/:path*`,
+//         },
+//       ],
+//     }
+//   },
+// };
+
 module.exports = nextConfig;
